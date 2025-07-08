76dc541161346d5b588d5d20b4585271ef8075ba
brayschway <bradanschwanke@gmail.com>
2025-07-08 09:08:03 -0600

    Finished video four. We just made links to new pages for when you click on a category or search a prodcuct from the home page but have not yet included content for each of those pages.

--- webapp/src/app/components/header/header.ts
+++ webapp/src/app/components/header/header.ts
@@ -1,14 +1,28 @@
 import { Component, inject } from '@angular/core';
 import { CategoryService } from '../../services/category';
 import { Category } from '../../types/category';
+import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+
+searchCategory(id: string) {
+  this.router.navigateByUrl("/products?categoryId="+id);
+}
+
+router = inject(Router);
+onSearch(e: any) {
+  if (e.target.value)
+  {
+    this.router.navigateByUrl("/products?search="+e.target.value);
+  }
+}
 
   categoryService = inject(CategoryService);
   categoryList:Category[] = [];
